1f34214d1b023b0ac2aff7d6a80857a6c6c206b9
Ju Pan <jupan1994@gmail.com>
2018-04-03 20:27:33 -0700

    add code for retrieve data from database

--- src/com/jupan/hibernate/demo/ReadStudentDemo.java
+++ src/com/jupan/hibernate/demo/ReadStudentDemo.java
@@ -6,7 +6,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.jupan.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class ReadStudentDemo {
 
   public static void main(String[] args) {
 
@@ -22,16 +22,34 @@ public class CreateStudentDemo {
     try {     
       // create a student object
       System.out.println("Creating new student object...");
-      Student tempStudent = new Student("James", "Harden", "jupan1994@gmail.com");
+      Student tempStudent = new Student("Conor", "McGregor", "mac@gmail.com");
       
       // start a transaction
       session.beginTransaction();
       
       // save the student object
       System.out.println("Saving the student...");
+      System.out.println(tempStudent);
       session.save(tempStudent);
       
       // commit transaction
+      session.getTransaction().commit();
+      
+      // find out the student's id: primary key
+      System.out.println("Saved student. Generated id: " + tempStudent.getId());
+      
+      // get a new session and start transaction
+      session = factory.getCurrentSession();
+      session.beginTransaction();
+      
+      // retrieve student based on the id: primary key
+      System.out.println("\nGetting student with id: " + tempStudent.getId());
+      
+      Student myStudent = session.get(Student.class, tempStudent.getId());
+      
+      System.out.println("Get complete: " + myStudent);
+      
+      // commit the transaction
       session.getTransaction().commit();
       
       System.out.println("Done!");
